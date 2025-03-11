3f1fedd134d32e2ef0203036b066e03dc70f7028
DarrylMathias <mathiasndarryl7@gmail.com>
2025-03-11 13:09:18 +0530

    Finals

--- Darryl/gsap.js
+++ Darryl/gsap.js
@@ -29,3 +29,9 @@ t1.from('.man', {
         markers: false,
     }
 })
+gsap.from(".grid-hero h1",{
+
+})
+gsap.from(".card1",{duration : 1, x : "-100vw", ease : 'powe3.out',opacity: 0}, 1)
+gsap.from(".card2",{duration : 1, delay:1, y : "100vw", ease : 'powe3.out', opacity: 0}, 1)
+gsap.from(".card3",{duration : 1, x : "100vw", ease : 'powe3.out',opacity: 0}, 1)
